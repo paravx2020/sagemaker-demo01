b380b88b500c816a787fa75b836fbe80e861f9e1
Ven Para <paravx@gmail.com>
2024-04-29 12:42:15 +0100

    adding python pkg for lambda function

---
+++ scripts/lambda_pkg.py
@@ -0,0 +1,17 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    runtime_client  = boto3.client('runtime.sagemaker')
+    endpoint_name   = 'xgboost-2024-xxxxxxxxxxxxxxx'
+    
+    sample          = '1.2, 2.2, 3.3, 4.4'
+    response        = runtime_client.invoke_encpoint(EndpointName=endpoint_name, ContentType='text/csv', Body=sample)
+    result          = response['Body'].read().decode('ascii')
+
+    print(result)
+
+    retunr {
+       'statusCode': 200,
+       'body': json.dumps({'prediction': result}) 
+    }
