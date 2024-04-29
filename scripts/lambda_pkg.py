ec05d82090aa9eddd825881cb21a4a09a1df6ab7
Ven Para <paravx@gmail.com>
2024-04-29 13:09:36 +0100

    updating lambda_pkg.py

--- scripts/lambda_pkg.py
+++ scripts/lambda_pkg.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     runtime_client  = boto3.client('runtime.sagemaker')
     endpoint_name   = 'xgboost-2024-xxxxxxxxxxxxxxx' ## to be replaced by actual 
     
-    sample          = '1.2, 2.2, 3.3, 4.4'
+    sample          = '7.7, 2.8, 6.7, 2.0'
     response        = runtime_client.invoke_encpoint(EndpointName=endpoint_name, ContentType='text/csv', Body=sample)
     result          = response['Body'].read().decode('ascii')
 
