d788caa3209ea96b7a4097e4103f134d453d8deb
Ven Para <paravx@gmail.com>
2024-04-29 16:39:20 +0100

    refactoring code

--- scripts/lambda_pkg.py
+++ scripts/lambda_pkg.py
@@ -1,11 +1,17 @@
 import boto3
 import json
+import ast
 
 def lambda_handler(event, context):
     runtime_client  = boto3.client('runtime.sagemaker')
     endpoint_name   = 'xgboost-2024-xxxxxxxxxxxxxxx' ## to be replaced by actual 
     
-    sample          = '7.7, 2.8, 6.7, 2.0'
+    #sample          = '7.7, 2.8, 6.7, 2.0'
+    sample          = '{},{},{},{}'.format(ast.literal_eval(event[body])['x1'],
+                                            ast.literal_eval(event[body])['x2'],
+                                            ast.literal_eval(event[body])['x3'],
+                                            ast.literal_eval(event[body])['x4'])
+                                            
     response        = runtime_client.invoke_encpoint(EndpointName=endpoint_name, ContentType='text/csv', Body=sample)
     result          = response['Body'].read().decode('ascii')
 
