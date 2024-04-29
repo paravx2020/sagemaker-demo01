113524ead704380354d99a7ec01d2cc36043c517
Ven Para <paravx@gmail.com>
2024-04-29 12:43:43 +0100

    adding lambda_pkg.py for lambda function

--- scripts/lambda_pkg.py
+++ scripts/lambda_pkg.py
@@ -3,7 +3,7 @@ import json
 
 def lambda_handler(event, context):
     runtime_client  = boto3.client('runtime.sagemaker')
-    endpoint_name   = 'xgboost-2024-xxxxxxxxxxxxxxx'
+    endpoint_name   = 'xgboost-2024-xxxxxxxxxxxxxxx' ## to be replaced by actual 
     
     sample          = '1.2, 2.2, 3.3, 4.4'
     response        = runtime_client.invoke_encpoint(EndpointName=endpoint_name, ContentType='text/csv', Body=sample)
