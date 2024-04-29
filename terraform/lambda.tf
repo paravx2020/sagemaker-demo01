59febd8d2a73882b2d3a38e2bb28930ef1af9c85
Ven Para <paravx@gmail.com>
2024-04-29 13:07:15 +0100

    adding lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "lambda" {
   filename = "index.zip"
   function_name = "paravx1-sagemaker-LambdaFunction"
   role = aws_iam_role.lambda.arn
-  handler = "index.handler"
+  handler = "lambda_handler"
   runtime = "python3.12"
   source_code_hash = data.archive_file.lambda.output_base64sha256
 }
